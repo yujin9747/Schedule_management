b68e34fad78d2bc6d5ccfbb681d0f4c9df794ca2
yujin9747 <jyj9747@naver.com>
2022-09-29 17:32:50 +0900

    route 세팅 : home, recharge 페이지 연결

---
+++ lib/Home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('route 테스트 : Home입니다.'),
+    );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'Home.dart';
+import 'Recharge.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -12,19 +13,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Test for commit and push',
+      initialRoute: '/recharge',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/' : (context) => Home(),
+        '/recharge':(context) => Recharge(),
+      },
     );
   }
 }
