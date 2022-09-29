3d3e2dd47cc99f57b567d2f1337ffd88f1d35baf
yujin9747 <jyj9747@naver.com>
2022-09-29 17:08:24 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Test for commit and push',
       theme: ThemeData(
         // This is the theme of your application.
         //
