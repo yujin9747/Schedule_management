c7b5dd333e860512cc3296ef9409a191ceb6a2d3
yujin9747 <jyj9747@naver.com>
2022-10-06 14:16:47 +0900

    recharge.dart : card/list 버튼 생성 및 전환 구현

--- lib/Recharge.dart
+++ lib/Recharge.dart
@@ -2,7 +2,26 @@ import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
+// appBar style 통일하기
+
+class card{
+  late String assets;
+  late String name;
+  
+  card(String a, String n){
+    this.assets = a;
+    this.name = n;
+  }
+}
 class Recharge extends StatefulWidget{
+
+  // 기존에 있던 카드 리스트와 사용자가 추가한 리스트가 합쳐진 채로 recharge.dart 페이지가 넘어와야 함.
+  List<card> rechargeCardList = [   
+    card('assets/rechargeCard0.json', 'driving'),
+    card('assets/rechargeCard1.json', 'running'),
+    card('assets/rechargeCard2.json', 'coffee'),
+  ];
+  
   @override
   _RechargeState createState() {
     return new _RechargeState();
@@ -10,6 +29,8 @@ class Recharge extends StatefulWidget{
 }
 
 class _RechargeState extends State<Recharge>{
+  // initial state : card view
+  bool isCardView = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +42,7 @@ class _RechargeState extends State<Recharge>{
             children: [
               const SizedBox(width: 20,),
               Text(
-                '쉼 추가하기',
+                '쉼 일정\n추가하기',
                 style: TextStyle(
                   fontSize: 40,
                   fontWeight: FontWeight.bold,
@@ -29,7 +50,37 @@ class _RechargeState extends State<Recharge>{
               ),
             ],
           ),
-          Expanded(child: swipeCardBuilder()),
+          const SizedBox(height : 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: (){
+                  if(!isCardView){
+                    setState(() {
+                      isCardView = true;
+                    });
+                  }
+                  // if isCardView is true, nothing to do
+                },
+                child: Text('List'),
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  if(isCardView){
+                    setState(() {
+                      isCardView = false;
+                    });
+                  }
+                  // if isCardView is false, nothing to do
+                },
+                child: Text('List'),
+              ),
+            ],
+          ),
+          const SizedBox(height : 50),
+          isCardView? Expanded(child: swipeCardBuilder(list: widget.rechargeCardList,))
+              : Expanded(child: cardListBuilder(list: widget.rechargeCardList,)),
         ],
       ),
     );
@@ -38,16 +89,45 @@ class _RechargeState extends State<Recharge>{
 }
 
 class swipeCardBuilder extends StatelessWidget{
+  List<card> list;
+  swipeCardBuilder({required this.list});
+
   @override
   Widget build(BuildContext context) {
+    // print List for test
+    printList(list);
     return Swiper(
-      itemCount:3,
+      itemCount:list.length,
       itemBuilder: (BuildContext context, int index) {
-        return Lottie.asset('assets/rechargeCard$index.json');
+        return Lottie.asset(list[index].assets);
       },
       viewportFraction: 0.8,
       scale: 0.5,
     );
   }
-
 }
+
+class cardListBuilder extends StatelessWidget{
+  List<card> list;
+  cardListBuilder({required this.list});
+
+  @override
+  Widget build(BuildContext context) {
+    // print List for test
+    printList(list);
+    return ListView.builder(
+      itemCount:list.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Lottie.asset(list[index].assets);
+      },
+    );
+  }
+}
+
+void printList(List<card> list){
+  print('Length of rechargeCardList : ${list.length}');
+  for(int i=0; i<list.length; i++){
+    print('Item ${i+1} : ${list[i].name}');
+  }
+}
+
