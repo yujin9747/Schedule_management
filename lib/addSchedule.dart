4626fc1281294769d2247e9baace82c329a2a50e
yujin9747 <jyj9747@naver.com>
2022-11-16 22:24:56 +0900

    addSchedule.dart : 폼 구성 구현 완료(디자인X). DB연결 작업 X.

--- lib/addSchedule.dart
+++ lib/addSchedule.dart
@@ -13,6 +13,7 @@ class AddSchedule extends StatefulWidget{
 
 class _AddSchedule extends State<AddSchedule>{
   final _formkey = GlobalKey<FormBuilderState>();
+  late bool timeSet = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,68 +22,174 @@ class _AddSchedule extends State<AddSchedule>{
         centerTitle: true,
         title: Text('Add Schedule'),
       ),
-      body: FormBuilder(
-        key: _formkey,
-        child: Column(
-          children: [
-            FormBuilderTextField(
-              name: 'title',
-              decoration: InputDecoration(
+      body: SingleChildScrollView(
+        child: FormBuilder(
+          key: _formkey,
+          child: Column(
+            children: [
+              const SizedBox(height: 20,),
+              FormBuilderTextField(
+                name: 'title',
+                decoration: InputDecoration(
                   hintStyle: TextStyle(color: Colors.grey[800]),
                   hintText: "Title",
+                  contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                  ),
+                  suffixIcon: IconButton(
+                    icon : Icon(Icons.clear),
+                    color: Colors.black38,
+                    onPressed: () {
+                      _formkey.currentState?.fields['title']?.reset();
+                    },
+                  ),
+                ),
+                validator: FormBuilderValidators.required(),
               ),
-              validator: FormBuilderValidators.required(),
-            ),
-            FormBuilderTextField(
-              name: 'memo',
-              decoration: InputDecoration(
-                hintStyle: TextStyle(color: Colors.grey[800]),
-                hintText: "memo",
+              const SizedBox(height: 20,),
+              FormBuilderTextField(
+                name: 'memo',
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(color: Colors.grey[800]),
+                  hintText: "memo",
+                  suffixIcon: IconButton(
+                    icon : Icon(Icons.clear),
+                    color: Colors.black38,
+                    onPressed: () {
+                      _formkey.currentState?.fields['memo']?.reset();
+                    },
+                  ),
+                ),
               ),
-            ),
-            // Date and Time picker
-            // FormBuilderDateTimePicker(
-            //     name: "start date",
-            // ),
-            // Date range picker
-            FormBuilderDateRangePicker(
-              name: "date_range",
-              firstDate: DateTime(2022),
-              lastDate: DateTime(2030),
-              format: DateFormat('yyyy-MM-dd'), // 2022-08-01
-              // onChanged: (){},
-              // decoration: InputDecoration(),
-            ),
-            FormBuilderSlider(
-              name: "rating",
-              initialValue: 0,
-              min: 0,
-              max: 10,
-              divisions: 10,
-              activeColor: Colors.blueAccent[100],
-              decoration: InputDecoration(
-                //hintText: '중요도',
+              const SizedBox(height: 20,),
+              FormBuilderDateRangePicker(
+                name: "date_range",
+                firstDate: DateTime(2022),
+                lastDate: DateTime(2030),
+                format: DateFormat('yyyy-MM-dd'), // 2022-08-01
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    icon : Icon(Icons.clear),
+                    color: Colors.black38,
+                    onPressed: () { _formkey.currentState?.fields['date_range']?.reset();},
+                  ),
+                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: (){
-                // Reset form
-                _formkey.currentState?.reset();
+              const SizedBox(height: 20,),
+              FormBuilderCheckbox(
+                name: 'timeSet',
+                title: Text('시간 설정'),
+                onChanged: (value){
+                  if(value == true){
+                    setState(() {
+                      timeSet = true;
+                    });
+                  }
+                  else{
+                    setState(() {
+                      timeSet = false;
+                      _formkey.currentState?.fields['start_time']?.reset();
+                      _formkey.currentState?.fields['start_time']?.didChange(null);
+                      _formkey.currentState?.fields['end_time']?.reset();
+                      _formkey.currentState?.fields['end_time']?.didChange(null);
+                    });
+                  }
+                },
+              ),
+              timeSet
+                  ? FormBuilderDateTimePicker(
+                name: 'start_time',
+                inputType: InputType.time,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    icon : Icon(Icons.clear),
+                    color: Colors.black38,
+                    onPressed: () {
+                      _formkey.currentState?.fields['start_time']?.reset();
+                      _formkey.currentState?.fields['start_time']?.didChange(null);
+                      },
+                  ),
+                ),
+              )
+                  : Container(),
+              timeSet
+                  ? FormBuilderDateTimePicker(
+                name: 'end_time',
+                inputType: InputType.time,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    icon : Icon(Icons.clear),
+                    color: Colors.black38,
+                    onPressed: () {
+                      _formkey.currentState?.fields['end_time']?.reset();
+                      _formkey.currentState?.fields['end_time']?.didChange(null);
+                      },
+                  ),
+                ),
+              )
+                  : Container(),
+              FormBuilderSlider(
+                name: "rating",
+                initialValue: 0,
+                min: 0,
+                max: 10,
+                divisions: 10,
+                activeColor: Colors.blueAccent[100],
+                decoration: InputDecoration(
+                  //hintText: '중요도',
+                ),
+              ),
+              const SizedBox(height: 20,),
+              FormBuilderRadioGroup(
+                name: 'when',
+                options: <FormBuilderFieldOption>[
+                  FormBuilderFieldOption(
+                    value: 'when_today',
+                    child: Text('오늘 일정에 추가하기'),
+                  ),
+                  FormBuilderFieldOption(
+                    value: 'when_tomorrow',
+                    child: Text('내일 일정에 추가하기'),
+                  ),
+                  FormBuilderFieldOption(
+                    value: 'when_userSelect',
+                    child: FormBuilderDateTimePicker(
+                      name: 'when_selectedDate',
+                      inputType: InputType.date,
+                    ),
+                  ),
+                ],
+                validator: (value){
+                  FormBuilderValidators.required();
+                  if(value == 'when_userSelect' && _formkey.currentState?.fields['when_userSelect']?.value == null){
+                    return  "You should select the date";
+                  }
+                  return null;
+                },
+                initialValue: 'when_today',
+              ),
+              const SizedBox(height: 20,),
+              ElevatedButton(
+                onPressed: (){
+                  // Reset form
+                  _formkey.currentState?.reset();
 
-                // Unfocus
-                FocusScope.of(context).unfocus();
-              },
-              child: Text('Reset'),
-            ),
-          ],
+                  // Unfocus
+                  FocusScope.of(context).unfocus();
+                },
+                child: Text('Reset'),
+              ),
+              const SizedBox(height: 20,),
+            ],
+          ),
+          onChanged: (){print("Form has been changed");},
+          autovalidateMode: AutovalidateMode.disabled,
+          initialValue: {
+          },
         ),
-        onChanged: (){
-          print("Form has been changed");
-        },
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        initialValue: {
-        },
       ),
+      //body:
       floatingActionButton: FloatingActionButton.extended(
         heroTag: 'addSchedule',
         onPressed: () => submitAction(),
@@ -125,8 +232,47 @@ class _AddSchedule extends State<AddSchedule>{
               ElevatedButton(
                 child: Text("확인"),
                 onPressed: () {
+                  // Todo : upload to Database
                   final snackbar = SnackBar(content: Text("Success : 일정이 추가되었습니다"),);
                   ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                  Navigator.pop(context);
+                  showAddMoreDialog();
+                },
+              ),
+            ],
+          );
+        }
+    );
+  }
+
+  // show Dialog "일정을 더 추가하시겠습니까?"
+  void showAddMoreDialog() {
+    showDialog(
+        context: context,
+        barrierDismissible: false,  // 다른 화면 터치 X
+        builder: (BuildContext context){
+          return AlertDialog(
+            // 화면 모서리 둥글게 조절
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            // Dialog Main Title
+            title: Column(
+              children: <Widget>[
+                Text("일정을 더 추가하시겠습니까?"),
+              ],
+            ),
+            actions: <Widget>[
+              ElevatedButton(
+                child: Text("종료"),
+                onPressed: () {
+                  Navigator.pop(context); // pop dialog
+                  Navigator.pop(context);
+                },
+              ),
+              ElevatedButton(
+                child: Text("More"),
+                onPressed: () {
+                  _formkey.currentState?.reset(); // 새로운 정보 입력 위한 초기화
                   Navigator.pop(context);
                 },
               ),
@@ -135,6 +281,7 @@ class _AddSchedule extends State<AddSchedule>{
         }
     );
   }
+
 
   // action after submit button is clicked
   void submitAction() {
@@ -160,5 +307,4 @@ class _AddSchedule extends State<AddSchedule>{
       showConfirmDialog();
     }
   }
-
 }
