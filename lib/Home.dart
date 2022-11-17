403e7d3e2ed4901f0bd7b25622930d8af6e54a60
isaaclkh <isaaclkh9909@gmail.com>
2022-11-17 17:42:39 +0900

    Home에 퍼센테이지 추가, main에 route 추가, yaml 추가

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,12 +8,14 @@ Things to be done.
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:jiffy/jiffy.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:scheduling/Recharge.dart';
 
 DateTime now = DateTime.now();
 
 var yoil = DateFormat.E('ko_KR').format(now).toString();
 var format = DateFormat("dd/MM").format(now).toString();
-String formattedDate = format + yoil;
+final String formattedDate = format + yoil;
 
 class Home extends StatefulWidget{
   const Home({super.key});
@@ -31,8 +33,17 @@ class _Home extends State<Home>{
       appBar: AppBar(
         //title: Text(formattedDate),
         centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+
+        leading: Builder( // menu icon
+          builder: (context) => IconButton(
+            icon: Icon(Icons.menu, color: Colors.black,),
+            onPressed: () => Scaffold.of(context).openDrawer(), // open drawer
+          ),
+        ),
       ),
-      body: Text('route 테스트 : Home입니다.'),
+
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
@@ -42,6 +53,110 @@ class _Home extends State<Home>{
           ],
         ),
       ),
+
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        heroTag: 'addSchedule',
+        onPressed: () {
+          Navigator.pushNamed(context, '/addSchedule');
+        },
+      ),
+
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 30, right: 30, top: 15,),
+            child: Container(
+              width: 50,
+              height: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.purple,
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 18,),
+                    child: CircularPercentIndicator(
+                      radius: 95.0,
+                      lineWidth: 15.0,
+                      percent: 0.8,
+                      center: Text(
+                        "80%",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 40,
+                        ),
+                      ),
+                      progressColor: Colors.white,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 20, left: 20,),
+                      child: Text("data"),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 10, left: 20,),
+                      child: Text("data"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          InkWell(
+            child: Padding(
+              padding: EdgeInsets.only(left: 30, right: 30, top: 15,),
+              child: Container(
+                width: 50,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blue,
+                ),
+                child: Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(),
+                      child: Text("아직 내일의 일정이 등록되지 않았어요"),
+                    ),
+                  ),
+                ),
+              ),
+            onTap: (){
+              Navigator.pushNamed(context, '/addSchedule');
+            },
+          ),
+          InkWell(
+            child: Padding(
+              padding: EdgeInsets.only(left: 30, right: 30, top: 15,),
+              child: Container(
+                width: 50,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blue,
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(),
+                    child: Text("쉼 계획하기"),
+                  ),
+                ),
+              ),
+            ),
+            onTap: (){
+              Navigator.pushNamed(context, '/recharge');
+            },
+          ),
+        ],
+      ),
+
     );
   }
 
